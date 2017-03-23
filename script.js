369740f36cf768b33bf1508964a8a5796f07f4dd
Kari A Barry <Kari.A.Barry@stonybrook.edu>
2017-03-23 19:43:16 -0400

    Made a JSON file for the lobby, attempting to parse it and create the way markers using the Inspector option in A-Frame to find where they should go. No connected rooms exist, but each room will need a JSON file.

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 var photo = 0;
+var startingRoom = "lobby"
 
 $(function() {
-    loadSpaces();
-    /*
-    $(window).keydown(function(evt) {
-        if (evt.which == 37) {
-            photo = (photo + 29) % 30;
-        }
-        else if (evt.which == 39){
-            photo = (photo + 1) % 30;
-        }
-        $("#sky").attr("src", "img/" + (photo < 10 ? "0" : "") + photo + ".JPG");
-    });
-    */
+    loadSphere(startingRoom, 0);
+
 });
 
-function loadSpaces() {
-  $.getJSON("union.json", function (data) {
-      console.log(data);
-      //nextRoom = data.nextRoom;
-      //clickable = data.targets;
-      //$("#room").css("font-size", "100px");
-      //$("#roomSVG").load("img/" + data.roomImage, roomSvgLoad);
+function loadSphere(room, num) {
+  $.getJSON(room + ".json", function (data) {
+      //console.log(data);
+      //console.log(data.spheres[0].number);
+      //console.log(data.spheres[num].leftImg);
+      $("#sky").attr("src", "img/" + data.spheres[num].leftImg);
+      console.log(data.spheres[num].markers[0].length);
+
+      for (var i = 0; i < data.spheres[num].markers.length; i++){
+        console.log(data.spheres[num].markers[0]);
+        //console.log(data.spheres[num].markers[i]);
+        //var x = data.spheres[num].markers[i].x;
+        //console.log(x);
+        var plane =$('<a-plane src="#ground" height="3" width="3" position="0 1 -1" rotation="-90 0 0"></a-plane>')
+        $("a-scene").append(plane);
+      }
+
   })
 }
