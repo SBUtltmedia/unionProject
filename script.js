9e1a74735c68e2a78f57072634819ce752678cbc
Kari A Barry <Kevin.A.Barry@stonybrook.edu>
2017-03-23 15:55:13 -0400

    JSON structure finalized! Each room is a JSON object, divided into spheres which each have their own markers.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var photo = 0;
 
 $(function() {
+    loadSpaces();
+    /*
     $(window).keydown(function(evt) {
         if (evt.which == 37) {
             photo = (photo + 29) % 30;
@@ -10,4 +12,15 @@ $(function() {
         }
         $("#sky").attr("src", "img/" + (photo < 10 ? "0" : "") + photo + ".JPG");
     });
+    */
 });
+
+function loadSpaces() {
+  $.getJSON("union.json", function (data) {
+      console.log(data);
+      //nextRoom = data.nextRoom;
+      //clickable = data.targets;
+      //$("#room").css("font-size", "100px");
+      //$("#roomSVG").load("img/" + data.roomImage, roomSvgLoad);
+  })
+}
