66865349ba6c1d0d2bb9b31fef474aa134f1a2b2
Kari A Barry <Kari.A.Barry@stonybrook.edu>
2017-03-30 14:33:30 -0400

    If MEANINGFUL MARKERS  be something ye wish, then drop on the deck and flop like a… next room icon! :D

--- script.js
+++ script.js
@@ -12,15 +12,35 @@ function loadSphere(room, num) {
       //console.log(data.spheres[0].number);
       //console.log(data.spheres[num].leftImg);
       $("#sky").attr("src", "img/" + data.spheres[num].leftImg);
-      console.log(data.spheres[num].markers[0].length);
+      //console.log(data.spheres[num].markers.length);
 
       for (var i = 0; i < data.spheres[num].markers.length; i++){
-        console.log(data.spheres[num].markers[0]);
+        //console.log(data.spheres[num].markers[0]);
         //console.log(data.spheres[num].markers[i]);
         //var x = data.spheres[num].markers[i].x;
         //console.log(x);
-        var plane =$('<a-plane src="#ground" height="3" width="3" position="0 1 -1" rotation="-90 0 0"></a-plane>')
-        $("a-scene").append(plane);
+        makeMarker(data.spheres[num].markers[i]);
+      }
+
+      function makeMarker(mkr)
+      {
+
+        var rotation=Math.atan2(mkr.x,mkr.z) * (180 / Math.PI)+180;
+console.log(rotation)
+
+        var marker= document.createElement('a-image');
+        marker.setAttribute('position', {
+          x: mkr.x,
+          y: mkr.y,
+          z: mkr.z
+        });
+        marker.setAttribute('rotation', {
+          x: -90,
+          y:rotation
+        });
+        marker.setAttribute('src',  "nextMarker.png")
+        marker.setAttribute('transparent',  "true")
+        $("a-scene").prepend(marker)
       }
 
   })
