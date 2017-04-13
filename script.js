8a65dddf9ceacbe72f54ff4656e833d942c4d7e4
Kari A Barry <Kari.A.Barry@stonybrook.edu>
2017-04-13 16:11:55 -0400

    Cleaned up vestigial console.log lines to make the code more legible since it is now to be shared on Github

--- script.js
+++ script.js
@@ -11,19 +11,11 @@ function leftPad(num){
 
 function loadSphere(room, num) {
   $.getJSON(room + ".json", function (data) {
-      //console.log(data);
 
     $('.marker').remove();
     $('.preview').remove();
-      //console.log(data.spheres[0].number);
-      //console.log(data.spheres[num].leftImg);
       $("#sky1").attr("src", "img/" + data.spheres[num].leftImg);
       $("#sky2").attr("src", "img/" + data.spheres[num].rightImg);
-      //console.log(data.spheres[num].markers.length);
-      //console.log(data.spheres[num].markers[0]);
-      //console.log(data.spheres[num].markers[i]);
-      //var x = data.spheres[num].markers[i].x;
-      //console.log(x);
 
     data.spheres.forEach(function(val,index,array){
     (new Image()).src ="img/"+val.leftImg;
@@ -31,7 +23,6 @@ function loadSphere(room, num) {
     });
 
     data.spheres[num].markers.forEach(function(val,index,array){
-    //console.log(val.room)
     if(val.room){
         (new Image()).src ="img/"+val.room+"_"+leftPad(val.number+1)+"_Left.JPG";
         (new Image()).src ="img/"+val.room+"_"+leftPad(val.number+1)+"_Right.JPG"
@@ -48,7 +39,6 @@ function loadSphere(room, num) {
         loadSphere(room, $(evt.target).data( "num" ));
     }
     else {
-        //console.log($(evt.target).data( "room" ));
         loadSphere($(evt.target).data( "room" ), $(evt.target).data( "num" ));
     }
 
@@ -70,7 +60,6 @@ function loadSphere(room, num) {
           y:spin
         });
         marker.setAttribute('src',  "nextMarker.png")
-        //marker.setAttribute('transparent',  "true")
         marker.setAttribute("cursor-listener")
         marker.setAttribute("id","marker"+id)
         marker.setAttribute('data-num',mkr.number);
