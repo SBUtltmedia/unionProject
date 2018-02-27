a65310ea1bd8fe96a8535484a9672aeb28cac993
Kari Barry <kari.a.barry@stonybrook.edu>
2018-02-27 11:48:37 -0500

    actual script changes, Jesse is setting the markers from now in

--- img/cropper.sh
+++ img/cropper.sh
@@ -2,12 +2,13 @@ left=$1_Left.JPG
 right=$1_Right.JPG
 sides=( $left $right )
 rm cropped/*
-convert $left -crop 240x1200+3923+1176  cropped/$left
-convert $right -crop 240x1200+1260+1075  cropped/$right
-for i in "${sides[@]}"
-	#do convert $i -alpha set -virtual-pixel transparent -channel A -morphology Distance Euclidean:1,100\! +channel ${i%.JPG}.png
-	 do
-	   echo $i
-	   convert cropped/$i -alpha set -virtual-pixel transparent  -channel A -blur 0x30 -level 50%,100% +channel cropped/${i%.JPG}.png
-	   convert $i cropped/${i%.JPG}.png -geometry +50+50 -composite portalsRemoved/$i
-   done
+convert $left -crop 700x1200+1160+1075  cropped/$right
+convert $right -crop 300x1200+3923+1176  cropped/$left
+
+convert cropped/$right -alpha set -virtual-pixel transparent  -channel A -blur 0x30 -level 50%,100% +channel cropped/${right%.JPG}.png
+convert cropped/${right%.JPG}.png -resize 96.88% cropped/${right%.JPG}.png
+convert $right cropped/${right%.JPG}.png -geometry +1165+1110 -composite portalsRemoved/$right
+
+convert cropped/$left -alpha set -virtual-pixel transparent  -channel A -blur 0x30 -level 50%,100% +channel cropped/${left%.JPG}.png
+convert cropped/${left%.JPG}.png -resize 96.88% cropped/${left%.JPG}.png
+convert $left cropped/${left%.JPG}.png -geometry +3928+1211 -composite portalsRemoved/$left
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var startingRoom = "Lobby_100";
+var startingRoom = "vestibule_100A";
 var sceneEl;
 var markers;
 var currentLocation;
@@ -11,10 +11,21 @@ $(function() {
     o.x = sceneEl.querySelector('#camera').getAttribute('rotation').x;
     o.y = sceneEl.querySelector('#camera').getAttribute('rotation').y;
     o.radius = -11;
-    o.room= "";
-    o.number= "";
-    o.type=""
- console.log(JSON.stringify(o))
+    o.room = "";
+    o.number = "";
+    o.type = ""
+
+    var cameraEl = document.querySelector('#camera');
+       var cameraRot=cameraEl.getAttribute("rotation")
+       var tripodEl = document.querySelector('#tripod');
+          var tripodRot=tripodEl.getAttribute("rotation")
+       if (cameraRot){
+       cameraRotY= cameraRot.y
+     }
+     console.log(tripodRot.y+cameraRotY)
+
+    o.y = (sceneEl.querySelector('#camera').getAttribute('rotation').y)+tripodRot.y;
+    console.log(JSON.stringify(o))
 
   })
   sceneEl = document.querySelector('a-scene');
@@ -29,7 +40,7 @@ AFRAME.registerComponent('cursor-listener', {
       var marker = evt.target.id
       if (markers[marker].type == "walk") {
         var startingAngle = markers[marker].startingAngle;
-        loadSphere((markers[marker].room) != undefined ? (markers[marker].room) : currentLocation.room , markers[marker].number, startingAngle, "");
+        loadSphere((markers[marker].room) != undefined ? (markers[marker].room) : currentLocation.room, markers[marker].number, startingAngle, "");
         console.log(startingAngle);
 
       }
@@ -44,53 +55,65 @@ function leftPad(num) {
 
 function loadSphere(room, sphereNum, angle, startingImage) {
   //Start polar coordinate helper in log, returns data formatted in x, y and radius
-  var cameraRotY = 0
-  var cameraEl = document.querySelector('#camera');
-  var cameraRot = cameraEl.getAttribute("rotation")
-  if (cameraRot) {
-    cameraRotY = cameraRot.y
-  }
-  console.log(cameraRotY, angle, angle - cameraRotY)
-  if (!angle) {
-    sceneEl.querySelector('#tripod').setAttribute('rotation', {
-      x: 0,
-      y: 0,
-      z: 0
-    });
-  } else {
 
-    sceneEl.querySelector('#tripod').setAttribute('rotation', {
-      x: 0,
-      y: angle - cameraRotY,
-      z: 0
-    });
-  }
 
   $.getJSON(room + ".json", function(data) {
     currentLocation = data.spheres[sphereNum];
-    currentLocation.sphereNum=parseInt(sphereNum);
-     currentLocation.room = room;
-     markers=currentLocation.markers;
-      $('.marker').remove();
-      $('.preview').remove();
-      $("#sky1").attr("src", "img/" + data.spheres[sphereNum].leftImg);
-      $("#sky2").attr("src", "img/" + data.spheres[sphereNum].rightImg);
-      //var x = data.spheres[sphereNum].markers[i].x;
+    currentLocation.sphereNum = parseInt(sphereNum);
+    currentLocation.room = room;
+    markers = currentLocation.markers;
+    $('.marker').remove();
+    $('.preview').remove();
+    sceneEl.querySelector('#sky1').setAttribute("src", "img/" + data.spheres[sphereNum].leftImg);
+ $("#sky2").attr("src", "img/" + data.spheres[sphereNum].rightImg);
 
-      data.spheres.forEach(function(val, index, array) {
-        (new Image()).src = "img/" + val.leftImg;
-        (new Image()).src = "img/" + val.rightImg;
+ document.querySelector('#sky1').addEventListener('materialtextureloaded', function(){
+
+
+    var cameraRotY = 0
+    var cameraEl = document.querySelector('#camera');
+    var cameraRot = cameraEl.getAttribute("rotation")
+    if (cameraRot) {
+      cameraRotY = cameraRot.y
+    }
+    console.log(cameraRotY, angle, angle - cameraRotY)
+    if (!angle) {
+      sceneEl.querySelector('#tripod').setAttribute('rotation', {
+        x: 0,
+        y: 0,
+        z: 0
       });
+    } else {
 
-      data.spheres[sphereNum].markers.forEach(function(val, index, array) {
-        //console.log(val.room)
-        if (val.room) {
-          (new Image()).src = "img/" + val.room + "_" + leftPad(val.number + 1) + "_Left.JPG";
-          (new Image()).src = "img/" + val.room + "_" + leftPad(val.number + 1) + "_Right.JPG";
-        }
-        val.id=index;
-        makeMarker(val, index);
-      })
+      sceneEl.querySelector('#tripod').setAttribute('rotation', {
+        x: 0,
+        y: angle - cameraRotY,
+        z: 0
+      });
+    }
+
+
+
+  })
+
+
+
+    //var x = data.spheres[sphereNum].markers[i].x;
+
+    data.spheres.forEach(function(val, index, array) {
+      (new Image()).src = "img/" + val.leftImg;
+      (new Image()).src = "img/" + val.rightImg;
+    });
+
+    data.spheres[sphereNum].markers.forEach(function(val, index, array) {
+      //console.log(val.room)
+      if (val.room) {
+        (new Image()).src = "img/" + val.room + "_" + leftPad(val.number + 1) + "_Left.JPG";
+        (new Image()).src = "img/" + val.room + "_" + leftPad(val.number + 1) + "_Right.JPG";
+      }
+      val.id = index;
+      makeMarker(val, index);
+    })
 
 
     function makeMarker(mkr, id) {
@@ -103,17 +126,17 @@ function loadSphere(room, sphereNum, angle, startingImage) {
         }
         var markerHolder = document.createElement('a-entity');
         markerHolder.setAttribute("id", "markerHolder" + id)
-        var marker= document.createElement('a-image');
-        marker.setAttribute('src',  "nextMarker.png");
+        var marker = document.createElement('a-image');
+        marker.setAttribute('src', "nextMarker.png");
         marker.setAttribute('scale', '1.8 1.8 1.8');
         marker.setAttribute('position', {
           //IMPLEMENTED POLAR ROTATION
           x: 0,
-  	      y: 0,
-  	      z: mkr.radius
+          y: 0,
+          z: mkr.radius
         });
         marker.setAttribute("cursor-listener", "")
-        marker.setAttribute("id",  id)
+        marker.setAttribute("id", id)
         marker.setAttribute("class", "marker")
         document.querySelector('#markers').appendChild(markerHolder)
         document.querySelector('#markerHolder' + id).appendChild(marker)
@@ -126,6 +149,6 @@ function loadSphere(room, sphereNum, angle, startingImage) {
     }
 
   }).fail(function(event, jqxhr, exception) {
-  console.log("JSON File does not exist");
-})
+    console.log("JSON File does not exist");
+  })
 }
